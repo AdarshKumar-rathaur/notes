a80cec4fb37a5276f10f7f39e2e232bff9d4ffc5
AdarshKumar-rathaur <72179963+AdarshKumar-rathaur@users.noreply.github.com>
2025-08-07 13:48:58 +0530

    Initial commit

---
+++ src/Components/NavbarNotes.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function NavbarNotes({setSearchTerm, searchTerm}) {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Notes</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" to="#">Home</a>
+                        </li>
+                    </ul>
+                    <form className="d-flex" role="search">
+                        <input className="form-control me-2" id="searchTxt" type="text" placeholder="Search"
+                            value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value.toLowerCase())} />
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    )
+}
